630a249324b16f47bc573dba324b1597d9176106
Administrator <1965270014@qq.com>
2024-12-07 01:31:38 +0800

    自动docker run

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 cp ../config.py ./tests_huobi/ &&
 tar -cvf huobi_python_contract-main.tar . &&
 docker build -t get_coin_statistic . &&
-docker run -it -v /usr/local/output:/output get_coin_statistic bash
+docker run -it -v /usr/local/output:/output get_coin_statistic
